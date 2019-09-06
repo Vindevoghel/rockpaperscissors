83d82c97d196b148004bd835a4bd08cf808e4a6a
Yannick Vindevoghel <vindevoghel@gmail.com>
2019-09-06 13:02:29 +0200

    figured out how to use switch statement

--- script.js
+++ script.js
@@ -34,7 +34,18 @@ resetButton.addEventListener("click", function(){
 pcButton.addEventListener("click", function() {
    pcChoice = Math.floor(Math.random() * 3);
    console.log(pcChoice);
-   playRound(playerChoice, pcChoice)
+   switch (gameChoice(playerChoice, pcChoice)) {
+    case 0:
+         announcer.innerText = options[playerChoice] + " matches " + options[pcChoice] + ". It's a tie! Score is " + playerScore + " vs " + pcScore;
+         break;
+     case 1:
+         ++playerScore;
+         announcer.innerText = options[playerChoice] + " beats " + options[pcChoice] + ". You win! Score is " + playerScore + " vs " + pcScore;
+         break;
+     case 2:
+            ++pcScore;
+            announcer.innerText = options[pcChoice] + " beats " + options[playerChoice] + ". You lose! Score is " + playerScore + " vs " + pcScore;
+         }
 });
 
 function reset() {
@@ -44,10 +55,11 @@ function reset() {
     pcScore = 0;
 }
 
-function playRound(playerChoice, pcChoice, gameSelection){
-    gameSelection = (playerChoice - pcChoice);
+function gameChoice(playerChoice, pcChoice){
+    gameSelection = (playerChoice - pcChoice) % 3;
     console.log(gameSelection);
-    if ((gameSelection%3) === 1){
+    return gameSelection;
+    /*if ((gameSelection%3) === 1){
         ++playerScore;
         announcer.innerText = options[playerChoice] + " beats " + options[pcChoice] + ". You win! Score is " + playerScore + " vs " + pcScore;
     } else if ((gameSelection%3) === 2){
@@ -55,6 +67,6 @@ function playRound(playerChoice, pcChoice, gameSelection){
         announcer.innerText = options[pcChoice] + " beats " + options[playerChoice] + ". You lose! Score is " + playerScore + " vs " + pcScore;
     } else if (gameSelection === 0){
         announcer.innerText = options[playerChoice] + " matches " + options[pcChoice] + ". It's a tie! Score is " + playerScore + " vs " + pcScore;
-    }
+    }*/
 }
 
